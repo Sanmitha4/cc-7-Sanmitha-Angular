f3b5e7effa85c187a071b57d123215448f843fa7
Sanmitha4 <sanmithashetty2004@gmail.com>
2026-04-13 12:30:35 +0000

    Angular App implementation

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,20 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Home } from './components/home/home';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  imports: [RouterOutlet,Home],
+  //templateUrl: './app.html',
+  template:`<h1>Hello Angular</h1>`,
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('property-app');
+  //protected readonly title = signal('property-app');
+  protected readonly title=("property-qpp");
+
+  //setTimeout ,event listener is fired or a promise got resolved /rejected,network call
+  // ngOnInit(){
+  //   console.log('App component was instantiated!');
+  //   this.title='propert app reloaded'
+  // }
 }
