959bb021d6bd52614145538d0da1626d21ed521c
Sanmitha4 <sanmithashetty2004@gmail.com>
2026-04-16 06:21:35 +0000

    Demonstarte inhecting a mock service

--- src/app/components/housing-location/housing-location.ts
+++ src/app/components/housing-location/housing-location.ts
@@ -1,14 +1,17 @@
 import { Component, input, output } from '@angular/core';
 import { HousingLocationInfo } from '../../models/housing-location-info';
+import { LocationService } from '../../services/location-service';
 
 @Component({
   selector: 'app-housing-location',
   templateUrl: './housing-location.html',
   styleUrls: ['./housing-location.css'],
+  
 })
 export class HousingLocation {
   housingLocation = input.required<HousingLocationInfo>();
   onLocationClick =output<HousingLocationInfo>();
+  locationService=inject(LocationService);
 
   handleButtonClick(event:MouseEvent){
     console.log(`${this.housingLocation().name} is clicked`);
