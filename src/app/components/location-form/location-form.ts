cfbf5f018bb8ddfc3c88e07a0875c9aa0a185812
Sanmitha4 <sanmithashetty2004@gmail.com>
2026-04-23 11:33:51 +0000

    Add a form to the panel, using which you can add/edit the location

--- src/app/components/location-form/location-form.ts
+++ src/app/components/location-form/location-form.ts
@@ -1,40 +1,19 @@
 import { Component, signal, inject } from '@angular/core';
 import { Router } from '@angular/router';
-import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
-import { LocationService } from '../../services/location-service';
-import { HousingLocationInfo } from '../../models/housing-location-info';
+import { FormsDemo } from '../forms-demo/forms-demo';
 
 @Component({
   selector: 'app-location-form',
-  imports: [ReactiveFormsModule],
+  imports: [FormsDemo],
   templateUrl: './location-form.html',
   styleUrl: './location-form.css',
 })
 export class LocationForm {
   shouldShowPanel = signal<boolean>(false);
   router = inject(Router);
-  formBuilder = inject(FormBuilder);
-  locationService = inject(LocationService);
-
-  locationForm!: FormGroup;
-  isEditMode = signal<boolean>(false);
 
   ngOnInit() {
-    this.initializeForm();
     this.showPanel();
-  }
-
-  initializeForm() {
-    this.locationForm = this.formBuilder.group({
-      id: [0],
-      name: ['', [Validators.required, Validators.minLength(3)]],
-      city: ['', [Validators.required, Validators.minLength(2)]],
-      state: ['', [Validators.required, Validators.minLength(2)]],
-      photo: ['', Validators.required],
-      availableUnits: [0, [Validators.required, Validators.min(0)]],
-      wifi: [false],
-      laundry: [false],
-    });
   }
 
   showPanel() {
@@ -44,23 +23,5 @@ export class LocationForm {
   hidePanel() {
     this.shouldShowPanel.set(false);
     this.router.navigate(['home']);
-  }
-
-  onSubmit() {
-    if (this.locationForm.valid) {
-      const formData = this.locationForm.value;
-      if (this.isEditMode()) {
-        this.locationService.updateLocation(formData);
-      } else {
-        this.locationService.addLocation(formData);
-      }
-      this.locationForm.reset();
-      this.hidePanel();
-    }
-  }
-
-  onCancel() {
-    this.locationForm.reset();
-    this.hidePanel();
   }
 }
